c149e27cec6a2ad03b8b6db13cd22679405b48b6
Renzo Meister <rm@jamotion.ch>
2022-07-18 14:26:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pydevd-odoo',
+    name='pydevd-flectra',
     version='1.1',
-    description='PyDev.Debugger plugin for Odoo',
-    url='https://github.com/odoo-ide/pydevd-odoo',
-    author='Trinh Anh Ngoc',
-    author_email='atw1990@gmail.com',
+    description='PyDev.Debugger plugin for Flectra',
+    url='https://github.com/jamotion/pydevd-flectra',
+    author='Jamotion',
+    author_email='info@jamotion.ch',
     packages=find_packages(),
     namespace_packages=['pydevd_plugins.extensions'],
     license='MIT',
